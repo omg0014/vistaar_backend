f910889039bc02f5d1c361992e7c7b2df5ffbaa2
Om Gupta <intern2@yugam.co>
2026-07-05 00:19:31 +0530

    Added bookmark

---
+++ controllers/bookmarkController.js
@@ -0,0 +1,59 @@
+'use strict';
+const getDb = require('../config/db');
+const { ObjectId } = require('mongodb');
+
+const COLL = 'bookmarks';
+
+async function getCollections(req, res, next) {
+  try {
+    const db = await getDb();
+    const cols = await db.collection(COLL).find({}).toArray();
+    res.json(cols);
+  } catch (err) { next(err); }
+}
+
+async function createCollection(req, res, next) {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ error: 'name required' });
+    const db = await getDb();
+    const result = await db.collection(COLL).insertOne({ name, schools: [], createdAt: new Date() });
+    res.json({ _id: result.insertedId, name, schools: [] });
+  } catch (err) { next(err); }
+}
+
+async function deleteCollection(req, res, next) {
+  try {
+    const db = await getDb();
+    await db.collection(COLL).deleteOne({ _id: new ObjectId(req.params.id) });
+    res.json({ success: true });
+  } catch (err) { next(err); }
+}
+
+async function addSchool(req, res, next) {
+  try {
+    const { school } = req.body;
+    const db = await getDb();
+    const existing = await db.collection(COLL).findOne({ _id: new ObjectId(req.params.id), 'schools._id': school._id });
+    if (!existing) {
+      await db.collection(COLL).updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $push: { schools: school } }
+      );
+    }
+    res.json({ success: true });
+  } catch (err) { next(err); }
+}
+
+async function removeSchool(req, res, next) {
+  try {
+    const db = await getDb();
+    await db.collection(COLL).updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $pull: { schools: { _id: req.params.schoolId } } }
+    );
+    res.json({ success: true });
+  } catch (err) { next(err); }
+}
+
+module.exports = { getCollections, createCollection, deleteCollection, addSchool, removeSchool };
